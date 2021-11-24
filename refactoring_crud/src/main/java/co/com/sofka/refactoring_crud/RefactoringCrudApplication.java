b27949b5f497c4f0e6952f59676651c2c733bc63
DaironPerilla <perrynikon@gmail.com>
2021-11-24 17:35:57 -0500

    v0.0.0 Esqueleto proyecto

---
+++ refactoring_crud/src/main/java/co/com/sofka/refactoring_crud/RefactoringCrudApplication.java
@@ -0,0 +1,13 @@
+package co.com.sofka.refactoring_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefactoringCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefactoringCrudApplication.class, args);
+	}
+
+}
